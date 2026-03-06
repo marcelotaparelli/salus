bf37f9ee298a3b69fba5f786ac1a86cbc6b7e3a0
EVAG <mvtaparelli@gmail.com>
2026-03-06 17:18:37 -0300

    fix: corrige bug do eslint

---
+++ lint-staged.config.mjs
@@ -0,0 +1,10 @@
+import path from "path"
+
+const buildEslintCommand = (filenames) =>
+  `eslint --fix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`
+
+export default {
+  "*.ts": [buildEslintCommand, "prettier --write"],
+}
