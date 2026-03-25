9d115efd4b9fb5a8d8e397215935c08429291562
EVAG <mvtaparelli@gmail.com>
2026-03-25 14:44:02 -0300

    feat: começa implementação das exceções

--- src/domain/errors/domain-error.ts
+++
@@ -1,9 +0,0 @@
-export class DomainError extends Error {
-  public readonly statusCode;
-
-  constructor(message: string, statusCode: number = 400) {
-    super(message);
-    this.name = "DomainException";
-    this.statusCode = statusCode;
-  }
-}
