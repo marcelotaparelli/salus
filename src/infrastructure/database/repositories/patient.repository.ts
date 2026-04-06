443038f1f0e389089137629c1e0ce9b5bef9656a
EVAG <mvtaparelli@gmail.com>
2026-04-06 11:48:31 -0300

    refactor: use mapper to transit data in the infra layer

--- src/infrastructure/database/repositories/patient.repository.ts
+++ src/infrastructure/database/repositories/patient.repository.ts
@@ -1,36 +1,17 @@
 import { PatientRepository } from "@domain/patient/repositories/patient.repository";
 import { Patient } from "@domain/patient/entities/patient.entity";
 import { prisma } from "../prisma/client";
-import { Name } from "@domain/value-objects/name.vo";
-import { Cpf } from "@domain/value-objects/cpf.vo";
-import { Phone } from "@domain/value-objects/phone.vo";
-import { BirthDate } from "@domain/value-objects/birth-date.vo";
+import { PatientMapper } from "@infra/mappers/patient-persistence.mapper";
 
 export class PrismaPatientRepository implements PatientRepository {
   async save(patient: Patient): Promise<void> {
-    await prisma.patient.create({
-      data: {
-        id: patient.id,
-        name: patient.name.value,
-        cpf: patient.cpf.value,
-        phone: patient.phone.value,
-        birthDate: patient.birthDate.value,
-      },
-    });
+    const toPersistencePatient = PatientMapper.toPersistence(patient);
+    await prisma.patient.create({ data: toPersistencePatient });
   }
 
   async findAll(): Promise<Patient[]> {
     const result = await prisma.patient.findMany();
-    return result.map((p) =>
-      Patient.reconstitute({
-        id: p.id,
-        name: new Name(p.name),
-        cpf: new Cpf(p.cpf),
-        phone: new Phone(p.phone),
-        birthDate: new BirthDate(p.birthDate),
-        createdAt: p.createdAt,
-      }),
-    );
+    return result.map(PatientMapper.toDomain);
   }
 
   async findById(id: string): Promise<Patient | null> {
@@ -38,23 +19,17 @@ export class PrismaPatientRepository implements PatientRepository {
       where: { id: id },
     });
     if (!result) return null;
-    return Patient.reconstitute({
-      id: result.id,
-      name: new Name(result.name),
-      cpf: new Cpf(result.cpf),
-      phone: new Phone(result.phone),
-      birthDate: new BirthDate(result.birthDate),
-      createdAt: result.createdAt,
-    });
+    return PatientMapper.toDomain(result);
   }
 
   async update(patient: Patient): Promise<void> {
+    const toPersistencePatient = PatientMapper.toPersistence(patient);
     await prisma.patient.update({
       where: { id: patient.id },
       data: {
-        name: patient.name.value,
-        phone: patient.phone.value,
-        birthDate: patient.birthDate.value,
+        name: toPersistencePatient.name,
+        phone: toPersistencePatient.phone,
+        birthDate: toPersistencePatient.birthDate,
       },
     });
   }
